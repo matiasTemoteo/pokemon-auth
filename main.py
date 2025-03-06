672a9b586422c1e3478cc991ababe9fafc6ffdef
matias temoteo <matiasmartinstemoteo@gmail.com>
2025-03-06 16:32:25 -0300

    Adding https certs and config to run on https mode

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from website import create_app
+import os
+
+ASSETS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 app = create_app()
 
 if __name__ == '__main__':
+    context = ('certs/code.crt', 'certs/code.key')
     app.config['DEBUG'] = True
-    app.run()
+    app.run(ssl_context=context)
